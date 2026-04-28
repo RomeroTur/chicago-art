bdad35400932dfffec105788a3af66b53a11e5af
Romero <romero.turtureanu@gmail.com>
2026-04-29 00:14:37 +0200

    started useLocalStorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -2,9 +2,25 @@ import type { ArtworkProps } from "../schemas/artwork";
 import { useState } from "react";
 
 const useLocalStorage = () => {
-	const [items, setItems] = useState<ArtworkProps[]>([]);
+	const [items, setItems] = useState<ArtworkProps[]>(() => {
+		const itemsStoredString: string | null =
+			localStorage.getItem("itemsStored");
 
-	function addItem(Obj: ArtworkProps) {}
+		if (!itemsStoredString) return [];
+
+		try {
+			return JSON.parse(itemsStoredString);
+		} catch {
+			return [];
+		}
+	});
+
+	function addItem(Obj: ArtworkProps) {
+		setItems((prevItems) => {
+			const isIn = prevItems.some((item) => Obj.id === item.id);
+			return !isIn ? [...prevItems, Obj] : [...prevItems];
+		});
+	}
 
 	function removeItem(Obj: ArtworkProps) {}
 
